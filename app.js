056fc59d7d14c6dbe7638ed0f9841d028665ed1e
zhans04Aq <145835709+zhans04Aq@users.noreply.github.com>
2023-12-18 20:50:04 +0500

    added new files to rep

---
+++ app.js
@@ -0,0 +1,11 @@
+import {random, uniq} from 'lodash'
+import axios from 'axios'  
+
+const num = [1,2,34,5,6,76,544,2]
+
+console.log(uniq(num))
+
+const name = 'zhans'
+for(const letter of name){
+    console.log('letter > '. letter)
+}
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    devtool : 'source-map',
+    entry :{
+        filename: './app.js'
+    },
+    output: {
+        filename: './bundle.js'
+    }
+}
